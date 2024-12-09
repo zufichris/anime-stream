5060b9913e38405f2559db597976135a18d7bf5b
zufichris <zufichris@gmail.com>
2024-12-10 01:25:01 +0400

    types

---
+++ @types/anime.ts
@@ -0,0 +1,20 @@
+export interface IAnime {
+  id: number | string;
+  title: string;
+  imgLg: string;
+  imgMd: string;
+  description: string;
+}
+export interface IAnimeList {
+  page: number;
+  limit: number;
+  hasNextPage: boolean;
+  list: IAnime[];
+}
+export interface IEpisode {
+  id: string;
+  title: string;
+  stream?: string;
+  duration: string;
+  thumbnail: string;
+}
