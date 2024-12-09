63d09e0e1e65e9bc3ae68255224e9f72f3769e1f
zufichris <zufichris@gmail.com>
2024-12-10 01:38:10 +0400

    public

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s4.anilist.co", // Correct hostname
+        pathname: "/file/**", // Allow all paths under /file
+      },
+    ],
+  },
+};
 
 export default nextConfig;
