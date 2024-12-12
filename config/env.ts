95ecce8db89bc003fb4dbadfee82ea4626bb8ff1
zufichris <zufichris@gmail.com>
2024-12-12 12:48:49 +0400

    Env config

--- config/env.ts
+++ config/env.ts
@@ -1,3 +1,8 @@
+// In lib/env.ts or utils/env.ts
+if (!process.env.NEXT_PUBLIC_MongoDB_URI) {
+  throw new Error("MongoDB_URI is not defined in environment variables");
+}
+
 export const env = {
-  db_uri: process.env.MongoDB_URI,
-};
+  db_uri: process.env.NEXT_PUBLIC_MongoDB_URI as string,
+} as const;
